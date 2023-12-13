7b585873ceb61bb43a98ff654a4dc40bef06647c
ZidanAlfarizaPutraPratama <zidanalfariza@gmail.com>
2023-12-13 16:44:30 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors;
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
