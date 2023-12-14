6bee32e7d48b392107bb176c5d77507a27cb04cf
ZidanAlfarizaPutraPratama <zidanalfariza@gmail.com>
2023-12-14 17:31:18 +0700

    update code

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,36 @@
+import { Level } from '../schema/user.schema';
+import { IsString, IsNumber, IsEnum, IsNotEmpty } from 'class-validator';
+
+export class UserDto {
+  @IsString()
+  @IsNotEmpty()
+  user_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  no_telepon: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  kuota: number;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nama_lengkap: string;
+
+  @IsString()
+  @IsNotEmpty()
+  jabatan: string;
+
+  @IsEnum(Level)
+  @IsNotEmpty()
+  level: Level;
+}
---
+++ src/schema/user.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export enum Level {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+@Schema({})
+export class User {
+  @Prop({ type: String, unique: true, required: true })
+  user_id: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  no_telepon: string;
+
+  @Prop()
+  kuota: number;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  nama_lengkap: string;
+
+  @Prop()
+  jabatan: string;
+
+  @Prop({
+    type: String,
+    enum: Level,
+  })
+  level: Level;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
